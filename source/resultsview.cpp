eab4792daff0413b37d4676c5bd346ab860fd4a6
Michele Colombi <michele.colombi@orobix.com>
2011-02-28 18:18:11 +0100

    pressureImage chnages dimensions

--- source/resultsview.cpp
+++ source/resultsview.cpp
@@ -1,5 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//   Copyright 2011 Mario Negri Institute & Orobix Srl
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 #include "resultsview.h"
 #include "ui_resultsview.h"
+
+#include <iostream>
+#include <QTextStream>
+using namespace std;
+
+QTextStream resultsout(stdout);
 
 ResultsView::ResultsView(QString cookie, QPixmap pressurePic, QPixmap flowPic, QString PMean,
                          QString FMean, QWidget *parent) :
@@ -7,13 +31,14 @@ ResultsView::ResultsView(QString cookie, QPixmap pressurePic, QPixmap flowPic, Q
     ui(new Ui::ResultsView)
 {
     ui->setupUi(this);
-    //QPixmap pressureImage = *pressurePic.scaledToWidth(50, Qt::SmoothTransformation);
-    QPixmap pressureImage = pressurePic.scaledToWidth(100, Qt::SmoothTransformation);
-    QPixmap flowImage= flowPic.scaledToWidth(100, Qt::SmoothTransformation);
-    ui->pressureLabel->setPixmap(pressureImage);
+    pressureImage = pressurePic;
+    flowImage = flowPic;
+    QPixmap shownPImage = pressureImage.scaledToWidth(50, Qt::SmoothTransformation);
+    QPixmap shownFImage= flowImage.scaledToWidth(50, Qt::SmoothTransformation);
+    ui->pressureLabel->setPixmap(shownPImage);
     ui->PValue->setText(PMean);
 
-    ui->flowLabel->setPixmap(flowImage);
+    ui->flowLabel->setPixmap(shownFImage);
     ui->FValue->setText(FMean);
 
     setResultsCookie(cookie);
@@ -22,6 +47,18 @@ ResultsView::ResultsView(QString cookie, QPixmap pressurePic, QPixmap flowPic, Q
 ResultsView::~ResultsView()
 {
     delete ui;
+}
+
+void ResultsView::resizeEvent(QResizeEvent *event)
+{
+    if ((ui->pressureLabel->height() + ui->flowLabel->height()) > height() * 0.8 ) {
+        return;
+    }
+
+    const QPixmap* temp = ui->pressureLabel->pixmap();
+    QPixmap newImage;
+    newImage = (*temp).scaledToWidth(width() * 0.8, Qt::SmoothTransformation);
+    ui->pressureLabel->setPixmap(newImage);
 }
 
 void ResultsView::okButtClicked()
--- source/resultsview.h
+++ source/resultsview.h
@@ -1,3 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//   Copyright 2011 Mario Negri Institute & Orobix Srl
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 #ifndef RESULTSVIEW_H
 #define RESULTSVIEW_H
 
@@ -20,6 +38,9 @@ private:
     Ui::ResultsView *ui;
     void setResultsCookie(QString theCookie);
 
+protected:
+    void resizeEvent(QResizeEvent *event);
+
 signals:
     void okButtonClicked(QString cookie);
     void deleteItself();
@@ -29,6 +50,7 @@ private slots:
 
 private:
     QString resultsCookie;
+    QPixmap pressureImage, flowImage;
 };
 
 #endif // RESULTSVIEW_H
